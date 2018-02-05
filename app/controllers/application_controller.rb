5919a616f51f7131d084b6cdffcf37e2a54a6fe5
Elle Faraday <faraday.elle@gmail.com>
2018-02-05 01:07:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    Post.create(params)
+    Post.create(name: params[:name], content: params[:content])
   end
 
   get '/posts' do
